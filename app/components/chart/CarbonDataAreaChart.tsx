68b5e6a1719e76a493df687c04bbe936808f7348
AidanLDev <dev@aidanlowson.com>
2023-11-12 20:59:45 +0000

    Styling

--- app/components/chart/CarbonDataAreaChart.tsx
+++ app/components/chart/CarbonDataAreaChart.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   AreaChart,
   Area,
@@ -10,26 +11,35 @@ import {
 import { ChartTooltip } from './ChartTooltip';
 import { getMaxCarbonAmount } from '@/app/lib/Utils';
 import { ICarbonDataAreaChartProps } from '@/app/interfaces/IChartTypes';
+import { useState } from 'react';
 
 export const CarbonDataAreaChart = ({
   chartData,
 }: ICarbonDataAreaChartProps) => {
+  const [showGrid, setShowGrid] = useState(false);
   return (
-    <AreaChart data={chartData} width={1200} height={720} margin={{ top: 10 }}>
-      <CartesianGrid strokeDasharray='3 3' />
+    <AreaChart
+      data={chartData}
+      width={1200}
+      height={720}
+      margin={{ top: 10 }}
+      onClick={() => setShowGrid((prevState) => !prevState)}
+    >
+      {showGrid && <CartesianGrid strokeDasharray='3 3' />}
       <XAxis dataKey='name' label={{ value: 'Week', dy: 20, dx: -24 }} />
       {/* Programmatically get highest amount of data to set graph hight */}
       <YAxis
         dataKey='carbonAmount'
         domain={[0, getMaxCarbonAmount(chartData)]}
+        label={{ value: 'Carbon (tCO2e)', angle: -90, dy: 20 }}
       />
       <Tooltip content={<ChartTooltip />} />
       <Legend wrapperStyle={{ bottom: '-20px' }} />
       <Area
         type='monotone'
         dataKey='carbonAmount'
-        stroke='#8884d8'
-        fill='#8884d8'
+        stroke='#FFF'
+        fill='#F36B56'
         name='Carbon Amount (tCO2e)'
       />
     </AreaChart>
